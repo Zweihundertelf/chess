7b35f8063e4b9f011f9d6a82f4db64ffa8656917
Patrick <echomabon@gmail.com>
2023-10-29 15:05:00 +0100

    add figures to board

--- components/utils/startPosition.ts
+++ components/utils/startPosition.ts
@@ -1,7 +1,12 @@
 import Piece from './Piece';
 import Player from './Player';
 
+import Bishop from './pieces/Bishop';
+import King from './pieces/King';
+import Knight from './pieces/Knight';
 import Pawn from './pieces/Pawn';
+import Queen from './pieces/Queen';
+import Rook from './pieces/Rook';
 
 const startPosition = () => {
   const figures: (Piece | null)[][] = Array.from(Array(8), () => new Array(8));
@@ -16,6 +21,27 @@ const startPosition = () => {
     figures[1][i] = new Pawn(Player.BLACK);
     figures[6][i] = new Pawn(Player.WHITE);
   }
+
+  figures[0][0] = new Rook(Player.BLACK);
+  figures[0][7] = new Rook(Player.BLACK);
+  figures[7][0] = new Rook(Player.WHITE);
+  figures[7][7] = new Rook(Player.WHITE);
+
+  figures[0][1] = new Knight(Player.BLACK);
+  figures[0][6] = new Knight(Player.BLACK);
+  figures[7][1] = new Knight(Player.WHITE);
+  figures[7][6] = new Knight(Player.WHITE);
+
+  figures[0][2] = new Bishop(Player.BLACK);
+  figures[0][5] = new Bishop(Player.BLACK);
+  figures[7][2] = new Bishop(Player.WHITE);
+  figures[7][5] = new Bishop(Player.WHITE);
+
+  figures[0][3] = new Queen(Player.BLACK);
+  figures[7][3] = new Queen(Player.WHITE);
+
+  figures[0][4] = new King(Player.BLACK);
+  figures[7][4] = new King(Player.WHITE);
 
   return figures;
 };
