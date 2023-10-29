1654004b4ffe03e41d0b7c5c6edb6d3e78c9302a
Patrick <echomabon@gmail.com>
2023-10-29 15:00:34 +0100

    create bishop

---
+++ components/utils/pieces/Bishop.ts
@@ -0,0 +1,9 @@
+import Piece from '@components/utils/Piece';
+import Player from '@components/utils/Player';
+
+export default class Bishop extends Piece {
+  constructor(player: number) {
+    super(player);
+    this.image = player === Player.WHITE ? 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg' : 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg';
+  }
+}
