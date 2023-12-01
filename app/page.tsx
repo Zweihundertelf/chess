7de697d26355ff44793b5b7043de3d8da5e3baf5
Patrick <echomabon@gmail.com>
2023-12-01 23:35:24 +0100

    add heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import React from 'react';
 import Board from '../components/board/Board';
 
+import styles from './index.module.scss';
+
 const IndexPage = () => {
-  return <Board />;
+  return (
+    <React.Fragment>
+      <div className={styles.heading}>
+        <h1>Chess Game</h1>
+      </div>
+      <Board />
+    </React.Fragment>
+  );
 };
 
 export default IndexPage;
